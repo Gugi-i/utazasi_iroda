92ee6194d929e5a96dc97ceac6af980edf595503
botondborocz <boroczboti17@gmail.com>
2025-11-30 10:46:50 +0100

    Added status for my rentals

--- frontend/car_rental/src/components/RentedCarCard.jsx
+++ frontend/car_rental/src/components/RentedCarCard.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import './RentedCarCard.css';
 
 function RentedCarCard({ car, onDelete }) {
+
+    const getStatus = (start, end) => {
+        const now = new Date();
+        const startDate = new Date(start.replace(/\.$/, '').replace(/\./g, '-'));
+        const endDate = new Date(end.replace(/\.$/, '').replace(/\./g, '-'));
+
+        now.setHours(0, 0, 0, 0);
+
+        if (now > endDate) {
+            return 'Completed';
+        } else if (now >= startDate && now <= endDate) {
+            return 'Ongoing';
+        } else {
+            return 'Upcoming';
+        }
+    };
+
+    const status = getStatus(car.rentalDate, car.returnDate);
+
     return (
         <div className="rental-card">
             <img src={car.imageUrl} alt={car.carModel} className="rental-card-img" />
@@ -10,15 +29,18 @@ function RentedCarCard({ car, onDelete }) {
                 <h3>{car.carModel}</h3>
                 <p className="rental-dates">{car.rentalDate} - {car.returnDate}</p>
 
-                {/* Status Badge logic */}
-                {/*<span className={`status-badge ${car.status.toLowerCase()}`}>*/}
-                {/*    {car.status}*/}
-                {/*</span>*/}
+                <span className={`status-badge ${status.toLowerCase()}`}>
+                    {status}
+                </span>
             </div>
-
-            <button className="cancel-btn" onClick={onDelete}>
+            {status === 'Upcoming' &&
+            <button
+                className="cancel-btn"
+                onClick={onDelete}
+            >
                 Cancel
             </button>
+            }
         </div>
     );
 }
